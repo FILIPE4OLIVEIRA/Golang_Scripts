1839050aa5afbc7c462628d70bb7fab7e05f70e7
FILIPE OLIVEIRA <55719753+FILIPE4OLIVEIRA@users.noreply.github.com>
2022-12-29 17:02:28 -0300

    Update main.go

--- main.go
+++ main.go
@@ -1,40 +1,34 @@
 package main
 
 import (
+	"NumericalMethods/Integration"
 	screen "fmt"
 	"math"
-	"metodosnumericos/Numerical"
 )
 
-func MathEquation(x float64) (point float64) {
-	yx := math.Exp(-3*x) * math.Sin(4*x)
-	return yx
+func MathEquation(x, y, z float64) (point float64) {
+	result := math.Sin(x * y * z)
+	return result
 }
 
 func main() {
-	var x0, x1, delta float64
+	var x0, x1, y0, y1, z0, z1 float64
 
-	x0 = -0.5
-	x1 = 0.5
-	delta = 0.00001
+	x0 = 0.1
+	x1 = 0.9
+	y0 = 0.2
+	y1 = 0.5
+	z0 = 0.4
+	z1 = 0.7
 
-	ZeroPoint1, erro1 := Numerical.Bisection_Method(x0, x1, delta, MathEquation)
-	ZeroPoint2, erro2 := Numerical.Newton_Raphson(x0, delta, MathEquation)
+	Integral, erro := Integration.Integral_Tripla(x0, x1, y0, y1, z0, z1, MathEquation)
 
-	if erro1 != nil {
-		screen.Printf("Bisection_Method \n")
-		screen.Printf("There is no root in this interview! \n")
+	if erro != nil {
+		screen.Printf("Integral_Dupla \n")
+		screen.Printf("Could not Itegrate in This Interview! \n")
 	} else {
-		screen.Printf("Bisection_Method \n")
-		screen.Printf("The root is: %.15v \n", ZeroPoint1)
-	}
-
-	if erro2 != nil {
-		screen.Printf("Newton_Raphson \n")
-		screen.Printf("There is no root in this interview! \n")
-	} else {
-		screen.Printf("Newton_Raphson \n")
-		screen.Printf("The root is: %.15v \n", ZeroPoint2)
+		screen.Printf("Integral_Dupla \n")
+		screen.Printf("The Integral is: %.15v \n", Integral)
 	}
 
 }
