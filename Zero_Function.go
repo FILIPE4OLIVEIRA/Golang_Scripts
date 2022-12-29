19b3db7d68478aa53864eceafeffc43c851f175f
FILIPE OLIVEIRA <55719753+FILIPE4OLIVEIRA@users.noreply.github.com>
2022-12-29 17:01:13 -0300

    Update and rename Numerical_Methods.go to Zero_Function.go

--- Zero_Function.go
+++ Zero_Function.go
@@ -1,6 +1,7 @@
-package Numerical
+package ZeroFunction
 
 import (
+	"fmt"
 	"math"
 )
 
@@ -34,12 +35,13 @@ func Bisection_Method(xIncial float64, xFinal float64, delta float64,
 		}
 		return average, erro
 	} else {
+		erro = fmt.Errorf("%d", 1)
 		return average, erro
 	}
 }
 
 func dydx(xIncial float64, MathFunction func(float64) float64) (tetha float64) {
-	var step float64 = 0.01
+	var step float64 = 0.001
 	tetha = (MathFunction(xIncial+step) - MathFunction(xIncial)) / (step)
 	return tetha
 }
@@ -68,5 +70,36 @@ func Newton_Raphson(xIncial float64, delta float64,
 		} else {
 			return xIncial, erro
 		}
+	}
+}
+
+func Secante_Method(xIncial float64, xFinal float64, delta float64,
+	MathFunction func(float64) float64) (ZeroPoint float64, erro error) {
+
+	var count int
+	var MaxInt int
+	var valueNext float64
+
+	MaxInt = 100
+	count = 1
+
+	if MathFunction(xIncial)*MathFunction(xFinal) < 0 {
+
+		valueNext = (xIncial + xFinal) / 2.0
+
+		for count < MaxInt && math.Abs(MathFunction(valueNext)) > delta {
+			valueNext = xIncial - ((MathFunction(xIncial) * (xIncial - xFinal)) / (MathFunction(xIncial) - MathFunction(xFinal)))
+			xIncial = xFinal
+			xFinal = valueNext
+			count = count + 1
+		}
+		if count > MaxInt {
+			return float64(MaxInt), erro
+		} else {
+			return valueNext, erro
+		}
+	} else {
+		erro = fmt.Errorf("%d", 1)
+		return valueNext, erro
 	}
 }
